2c4da0d8c25e8da913441e9e4c0ee7995f1d1164
QareerShah <queensyeda16@gmail.com>
2024-11-29 02:20:02 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,38 @@
 import Link from 'next/link'
 import React from 'react'
 
+const page = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", { method: "GET" });
 
+  interface IUser {
+    id: number,
+    name: string,
+    email: string,
+    phone: number,
+    website: string,
+  }
 
-const page  = async () => {
-
-      const res = await fetch("https://jsonplaceholder.typicode.com/users" , {method: "GET"})
-
-      interface IUser {
-          "id": number,
-          "name": string,
-           "email": string,
-           "phone" : number,
-          "website" : string,
-      }
-
-      const users : IUser[] = await res.json()
-
-      console.log(users)
+  const users: IUser[] = await res.json();
+  console.log(users);
 
   return (
-    <div>
+    <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {
-         users.map((user)=>{
-          return <div key={user.id}>
-                <Link href={`/users/${user.id}`}><h1>{user.name}</h1></Link>
-          </div>
-         })
+        users.map((user) => {
+          return (
+            <div key={user.id} className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
+              <Link href={`/users/${user.id}`}>
+                <h2 className="text-xl font-semibold text-gray-800 hover:text-blue-500">{user.name}</h2>
+              </Link>
+              <p className="text-gray-600">Email: {user.email}</p>
+              <p className="text-gray-600">Phone: {user.phone}</p>
+              <p className="text-gray-600">Website: {user.website}</p>
+            </div>
+          );
+        })
       }
-
-        
-    
-    
     </div>
-  )
+  );
 }
 
-export default page
-
-
+export default page;
